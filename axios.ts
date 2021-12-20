59cae4a37b5d93a99c8f1cec5ed7515c84c95051
JuanAlcalaBeMaster <juanalcala@bemaster.com>
2021-12-20 13:02:22 -0500

    refactory/axios and request changes

---
+++ axios.ts
@@ -0,0 +1,100 @@
+import axios, { AxiosResponse } from 'axios';
+
+interface AuthInterface {
+  token?: string;
+}
+
+interface BaseOptionRequest {
+  url?: string;
+  endpoint?: string;
+  data?: string;
+  headers?: object;
+  auth?: AuthInterface;
+}
+
+const headerObjectDefault: any = {
+  token: (token: string) => ({
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${token}`,
+  }),
+  default: () => ({
+    'Content-Type': 'application/json',
+  }),
+};
+
+export default class Request {
+  baseUrl: string;
+
+  constructor(baseUrl?: string) {
+    this.baseUrl = baseUrl || `${process.env.URL_BASE_REQUEST}`;
+  }
+
+  static setAuthorizationDefault(auth: AuthInterface) {
+    return headerObjectDefault[Object.keys(auth)[0]](auth.token);
+  }
+
+  async get(options: BaseOptionRequest) {
+    try {
+      let headersTemp: object;
+      if (options.headers) {
+        headersTemp = { ...options.headers };
+      } else {
+        headersTemp = options.auth
+          ? Request.setAuthorizationDefault(options.auth)
+          : headerObjectDefault.default;
+      }
+
+      const response: AxiosResponse = await axios.get(
+        `${options.url ? options.url : this.baseUrl + options.endpoint}`,
+        { ...headersTemp },
+      );
+      return response.data;
+    } catch (error) {
+      throw new Error(`${error}`);
+    }
+  }
+
+  async post(options: BaseOptionRequest) {
+    try {
+      let headersTemp: object;
+      if (options.headers) {
+        headersTemp = { ...options.headers };
+      } else {
+        headersTemp = options.auth
+          ? Request.setAuthorizationDefault(options.auth)
+          : headerObjectDefault.default;
+      }
+
+      const response: AxiosResponse = await axios.post(
+        `${options.url ? options.url : this.baseUrl + options.endpoint}`,
+        JSON.stringify(options.data) || {},
+        { ...headersTemp },
+      );
+      return response.data;
+    } catch (error) {
+      throw new Error(`Request Error: ${error}`);
+    }
+  }
+
+  async put(options: BaseOptionRequest) {
+    try {
+      let headersTemp: object;
+      if (options.headers) {
+        headersTemp = { ...options.headers };
+      } else {
+        headersTemp = options.auth
+          ? Request.setAuthorizationDefault(options.auth)
+          : headerObjectDefault.default;
+      }
+
+      const response: AxiosResponse = await axios.post(
+        `${options.url ? options.url : this.baseUrl + options.endpoint}`,
+        JSON.stringify(options.data) || {},
+        { ...headersTemp },
+      );
+      return response.data;
+    } catch (error) {
+      throw new Error(`Request Error: ${error}`);
+    }
+  }
+}
