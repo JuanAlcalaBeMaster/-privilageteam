8b0db545a4e72322e993219d041e52180765678c
JuanAlcalaBeMaster <juanalcala@bemaster.com>
2021-12-20 14:41:49 -0500

    refactory/axios and request changes

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+exports.Request = exports.ResponseDTO = void 0;
+var requestDto_1 = require("./requestDto");
+exports.ResponseDTO = requestDto_1["default"];
+var axios_1 = require("./axios");
+exports.Request = axios_1["default"];
--- requestDto.ts
+++ requestDto.ts
@@ -29,4 +29,4 @@ class ResponseDTO {
 
 }
 
-export = ResponseDTO;
+export default ResponseDTO;
