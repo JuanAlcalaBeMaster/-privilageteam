92d7fdf755c3a2835b6771eeb54158d85484653f
JuanAlcalaBeMaster <juanalcala@bemaster.com>
2022-05-21 17:39:29 -0500

    [REFACTORY] Modify flow errors

--- index.ts
+++ index.ts
@@ -3,5 +3,6 @@ import { HttpError } from "./src/lib/errorHandling";
 import ResponseDTO from "./src/lib/responseDto";
 import ActiveCampaign from "./src/services/ActiveCampaign";
 import EmailSMTP from "./src/services/Email";
+import ErrorController from "./src/lib/error";
 
-export { ResponseDTO, Request, ActiveCampaign, EmailSMTP, HttpError };
+export { ResponseDTO, Request, ActiveCampaign, EmailSMTP, HttpError, ErrorController };
